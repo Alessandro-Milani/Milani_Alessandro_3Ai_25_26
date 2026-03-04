ded139272be232f219cfa42e68f9f2cb7aa892e0
Alessandro-Milani <milani.alessandro.09@itisdalmine.edu.it>
2026-03-04 19:03:43 +0100

    Versione corretta

--- 05-03/Ricerca_dicotomica.py
+++ 05-03/Ricerca_dicotomica.py
@@ -1,21 +1,18 @@
-#restituisce true se l'elemento cercato è nella lista, False altrimenti
+# restituisce True se l'elemento cercato è nella lista, False altrimenti
 def RicercaDicotomica(lista, numero_da_cercare):
     i = 0
     f = (len(lista)-1)
    
-    while i!=f or lista[m]==numero_da_cercare:
-   
+    while i <= f:
         m = (i+f)//2
- 
         if lista[m] == numero_da_cercare:
             return True
         if lista[m] > numero_da_cercare:
-            f=m
+            f = m - 1
         elif lista[m] < numero_da_cercare:
-            i=m
+            i = m + 1        
     return False
- 
- 
-lista=[10,12,44,72,88,96,104,1000]
- 
-print(RicercaDicotomica(lista, 88))
+
+lista = [10,12,44,72,88,96,104,1000]
+
+print(RicercaDicotomica(lista, 45))  # True
